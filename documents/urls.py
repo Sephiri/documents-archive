93877003989fe39ef95479ffd30eedd821abc2d2
Sephiri <hezangelika@gmail.com>
2026-07-03 22:38:47 +0200

    add document search filter function

--- documents/urls.py
+++ documents/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.document_list, name="document_list"),
     path("<int:document_id>/", views.document_detail, name="document_detail"),
     path("<int:document_id>/file/", views.document_file, name="document_file"),
+    path("<int:document_id>/download/", views.document_download, name="document_download"),
 ]
